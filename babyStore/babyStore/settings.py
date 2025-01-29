7a59b69f355ef6f951bfe1ea76fee195e5655a55
Muhd Nihal <156279420+muhdnihal107@users.noreply.github.com>
2025-01-29 15:04:29 +0530

    Update settings.py

--- babyStore/babyStore/settings.py
+++ babyStore/babyStore/settings.py
@@ -48,8 +48,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
